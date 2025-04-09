4858396fa9191fa86856a42d3ee00acc05209872
Kovid Bhatt <kovidbhatt@Admins-MacBook-Pro.local>
2025-04-09 12:08:29 +0530

    changes in nats connector

--- natstemplate/nats-connector.go
+++ natstemplate/nats-connector.go
@@ -2,23 +2,31 @@ package natstemplate
 
 import (
 	"fmt"
-
+	"os"
 	"github.com/nats-io/nats.go"
+	"github.com/joho/godotenv"
+	"log"
 )
 
 func NatsConnector() (*nats.Conn, nats.JetStreamContext, error){
-	fmt.Println("Initializing connection to the server....")
-	nc, err := nats.Connect("nats://localhost:4222")
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: No .env file found")
+	}
 
+	natsURL := os.Getenv("NATS_URL")
+	natsUser := os.Getenv("NATS_USER")
+	natsPass := os.Getenv("NATS_PASS")
+
+	nc, err := nats.Connect(natsURL, nats.UserInfo(natsUser, natsPass))
 	if err != nil {
 		return nil, nil, fmt.Errorf("error connecting to NATS: %w", err)
 	}
 
 	js, err := nc.JetStream()
-
 	if err != nil {
 		nc.Close()
-		return nil, nil, fmt.Errorf("error initializing the jetstream: %w", err)
+		return nil, nil, fmt.Errorf("error initializing JetStream: %w", err)
 	}
 
 	return nc, js, nil
